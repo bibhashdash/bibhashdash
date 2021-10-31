cacea3623598b09b02fb3bef83834943f07bf03b
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-10-31 23:54:32 +0000

    added blog link to index

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const menuBox = document.querySelector(".menu-box");
+// const menuBox = document.querySelector(".menu-box");
+// const btn = document.querySelector(".btn");
+// const listItem = document.querySelectorAll(".list-item");
+// btn.addEventListener("click", function() {
+//     menuBox.classList.toggle("menu-box-open");
+//     menuBox.style.transition = "all 1s";
+//     listItem.forEach(function(element) {
+//         element.classList.toggle("list-item-open");
+//         element.style.transition = "all 0.8s";
+//     });
+// });
+
 const btn = document.querySelector(".btn");
-const listItem = document.querySelectorAll(".list-item");
 btn.addEventListener("click", function() {
-    menuBox.classList.toggle("menu-box-open");
-    menuBox.style.transition = "all 1s";
-    listItem.forEach(function(element) {
-        element.classList.toggle("list-item-open");
-        element.style.transition = "all 0.8s";
-    });
+    alert("you clicked me!");
 });
