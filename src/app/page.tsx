e33868f876c3cb91470b3705f82f508282e5644c
bibhashdash <b.dash@12os.co.uk>
2023-12-17 00:18:56 +0000

    housekeeping...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 'use client';
 
-import {ButtonMain} from "@/components/buttons/ButtonMain";
-import {TwitterIcon} from "@/icons/TwitterIcon";
-import {EmailIcon} from "@/icons/EmailIcon";
-import {LinkedInIcon} from "@/icons/LinkedInIcon";
-import {useClientDimensions} from "@/utilities/clientDimensions";
+export default function About() {
 
-export default function Home() {
-  useClientDimensions();
-  const handleButtonClick = () => {
-    console.log("hello")
-  }
   return (
     <main className="flex h-full py-4 flex-col items-center w-full">
 
