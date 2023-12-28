0ca332b2700cc0f4e9f9403b01176ff286c0dd02
bibhashdash <b.dash@12os.co.uk>
2023-12-28 09:58:09 +0000

    adding product hunt embed

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -15,24 +15,36 @@ export default function ProjectDetailsPage() {
       {
         project ? (
           <div className="flex flex-col items-center justify-start w-full px-4 md:px-12 md:gap-6 h-full pb-12">
-            <div className="flex gap-4 w-full">
-              {/*<BackIcon />*/}
+            <div className="flex gap-4 w-full items-center">
               <p className="w-full text-lg font-bold">{project.title}</p>
+              {
+                project.id === 3 && (
+                  <a
+                    href="https://www.producthunt.com/posts/perfect-score?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-perfect&#0045;score"
+                    target="_blank"><img
+                    src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=431627&theme=light"
+                    alt="Perfect&#0032;Score - Hangman&#0032;for&#0032;Football&#0032;team&#0032;names | Product Hunt"
+                    className="w-[250px] h-[54px]"
+                    /></a>
+                )
+              }
             </div>
             <div className="flex flex-col items-center justify-start w-full lg:grid lg:grid-cols-12 lg:gap-6">
-              <img alt="project screenshots" src={project.imageSource} className="rounded-lg col-span-6" />
+              <img alt="project screenshots" src={project.imageSource} className="rounded-lg col-span-6"/>
               <div className="col-span-6">
-                <ProjectQuickInfo portfolioItem={project} />
+                <ProjectQuickInfo portfolioItem={project}/>
               </div>
             </div>
             <div className="mt-6 flex flex-col gap-4">
               <p><b>Description:</b> {project.description}</p>
-              <a className="w-full text-start font-bold cursor-pointer flex flex-row underline" href={'/work'}><BackIcon /> Back to all projects</a>
+              <a className="w-full text-start font-bold cursor-pointer flex flex-row underline"
+                 href={'/work'}><BackIcon/> Back to all projects</a>
             </div>
           </div>
         ) : (
           <div className="w-full h-full">
-            <p>We are having some trouble loading this content for you. Please refresh the page or try again in a bit.</p>
+            <p>We are having some trouble loading this content for you. Please refresh the page or try again in a
+              bit.</p>
           </div>
         )
       }
--- src/components/PortfolioGalleryCard.tsx
+++ src/components/PortfolioGalleryCard.tsx
@@ -1,4 +1,5 @@
 import {PortfolioDataModel} from "@/data/portfolioData";
+import React from "react";
 
 interface PortfolioGalleryCardProps {
   portfolioItem: PortfolioDataModel
@@ -8,8 +9,21 @@ interface PortfolioGalleryCardProps {
 export const PortfolioGalleryCard = ({portfolioItem, onClick}: PortfolioGalleryCardProps) => {
   return (
     <div onClick={() => onClick(portfolioItem.id)} className="rounded-lg py-4 flex flex-col items-center gap-2 cursor-pointer shadow-md">
-      <img src={portfolioItem.imageSource} className="rounded-lg"/>
-      <p className="font-bold text-sm sm:text-lg">{portfolioItem.title}</p>
+      <img alt={'portfolio-project-screenshots'} src={portfolioItem.imageSource} className="rounded-lg"/>
+      <div className="flex gap-2 items-center">
+        <p className="font-bold text-sm sm:text-lg text-center">{portfolioItem.title}</p>
+        {
+          portfolioItem.id === 3 && (
+            <a
+              href="https://www.producthunt.com/posts/perfect-score?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-perfect&#0045;score"
+              target="_blank"><img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=431627&theme=light"
+              alt="Perfect&#0032;Score - Hangman&#0032;for&#0032;Football&#0032;team&#0032;names | Product Hunt"
+              className="w-[120px] h-[30px]"
+            /></a>
+          )
+        }
+      </div>
     </div>
   )
 }
