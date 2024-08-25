a5a0211e88b5807cad3c7d4944acd9b0aee5b5df
bibhashdash <b.dash@12os.co.uk>
2024-08-25 22:38:32 +0100

    modifying career data

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -11,7 +11,8 @@ export default function Career() {
       <div className="flex flex-col items-center justify-start w-full px-4 md:px-12 md:gap-6 h-full">
         <p className="w-full text-lg text-center md:text-left font-bold">The Story So Far</p>
         <div className="w-full flex flex-col gap-6 mt-2">
-          <p><b>August 2022 - Present: </b>Junior Software Engineer at Twelve Oaks Software</p>
+          <p><b>September 2024 - Present: </b>Software Developer at i-Finity</p>
+          <p><b>August 2022 - August 2024: </b>Junior Software Engineer at Twelve Oaks Software</p>
           <p><b>June 2020 - August 2022: </b>Learning to code and freelance website building</p>
           <p><b>November 2020 - August 2022: </b>BAME Wellbeing Coordinator at HEY Smile Foundation</p>
           <p><b>April 2017 - November 2020: </b>Asbestos Surveyor and Analyst at Lucion Services</p>
